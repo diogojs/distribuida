2b3a82303928fa884f1033b53d42c0ea911ff662
Diogo Junior de Souza <diogo.junior@grad.ufsc.br>
2018-06-11 11:29:30 -0300

    Fix Ring algorithm

--- ring.cpp
+++ ring.cpp
@@ -21,21 +21,29 @@ int main(int argc, char** argv) {
     unsigned int token = 0;
     FakeProcess proc{rank, 0};
 
+    double t1 = MPI_Wtime();
+    double t2 = MPI_Wtime();
+    while (t2 < t1+1) {
+        t2 = MPI_Wtime();
+    }
+
     if (rank == 0) {
         token = 1;
         proc.token(token);
+        printf("Process %d sending token to %d.\n", rank, (rank+1));
         MPI_Send(&token, 1, MPI_UNSIGNED, (rank+1)%size, 0, MPI_COMM_WORLD);
     }
 
     for (int i = 1; i < duration; i++) {
         int r = ((rank-1)<0)?(size-1):(rank-1);
-        //proc.update();
         MPI_Recv(&token, 1, MPI_UNSIGNED, r, MPI_ANY_TAG, MPI_COMM_WORLD, &st);
         proc.token(token);
         proc.update();
 
-        if (token)
+        if (token) {
+            printf("Process %d sending token to %d.\n", rank, (rank+1)%size);
             MPI_Send(&token, 1, MPI_UNSIGNED, (rank+1)%size, 0, MPI_COMM_WORLD);
+        }
         proc.token(0);
     }
 
