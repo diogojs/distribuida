283b82b42f2b618284716bf9a05da86ddb589d87
Diogo Junior de Souza <diogo.junior@grad.ufsc.br>
2018-06-11 10:02:20 -0300

    Fix CentralServer algorithm

--- centralserver.cpp
+++ centralserver.cpp
@@ -1,17 +1,15 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <vector>
+#include <chrono>
+#include <thread>
+
 
 #include "fakeprocess2.h"
 
 int main(int argc, char** argv) {
     int size, rank;
-    unsigned int duration = 10;
     MPI_Status st;
-
-    if(argc == 2){
-        duration = atoi(argv[1]);
-    }
 
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -24,40 +22,57 @@ int main(int argc, char** argv) {
 
     // Server/Lider
     if (rank == 0) {
+        // Notify all processes that server is on
+        printf("Initializing server...\n");
+        double t1 = MPI_Wtime();
+        double t2 = MPI_Wtime();
+        while (t2 < t1+3) {
+            t2 = MPI_Wtime();
+        }
+        for (unsigned i = 1; i < size; ++i) {
+            MPI_Send(&token, 1, MPI_UNSIGNED, i, TAG_SEND, MPI_COMM_WORLD);
+        }
         bool running = true;
         token = 1;
         std::vector<int> queue{};
+        unsigned msg = 0;
         while (running) {
             // Aguarda pedidos
-            MPI_Recv(&token, 1, MPI_UNSIGNED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &st);
+            printf("Server listening...\n");
+            MPI_Recv(&msg, 1, MPI_UNSIGNED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &st);
             int enquirer = st.MPI_SOURCE;
             int tag = st.MPI_TAG;
             if (tag == TAG_ASK) {
                 if (token) {
+                    printf("Server sending token to %d\n", enquirer);
                     MPI_Send(&token, 1, MPI_UNSIGNED, enquirer, TAG_SEND, MPI_COMM_WORLD);
                     token = 0;
                 }
                 else {
+                    printf("Server put %d in queue.\n", enquirer);
                     queue.push_back(enquirer);
                 }
             }
             if (tag == TAG_GIVEBACK) {
-                queue.erase(queue.begin());
+                printf("Server received token back from %d\n", enquirer);
                 token = 1;
             }
             if (queue.size() > 0 && token) {
+                printf("Server sending token to %d\n", queue.front());
                 MPI_Send(&token, 1, MPI_UNSIGNED, queue.front(), TAG_SEND, MPI_COMM_WORLD);
                 queue.erase(queue.begin());
                 token = 0;
             }
-            if (tag == TAG_EXIT)
+            if (tag == TAG_EXIT) {
+                printf("Server shutting down.\n");
                 running = false;
+            }
         }
     }
 
     // Other processes
     else {
-        for (int i = 1; i < duration; i++) {
+        while (true) {
             proc.update();
         }
     }
--- fakeprocess2.h
+++ fakeprocess2.h
@@ -15,21 +15,36 @@ public:
         _rate{rate}
     {
         srand(0);
+        // Wait confirmation that Server is on
+        if (_rank != 0) {
+            std::cout << "P" << _rank << ": waiting for server startup." << std::endl;
+            MPI_Recv(&_token, 1, MPI_UNSIGNED, 0, TAG_SEND, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            std::cout << "P" << _rank << ": working." << std::endl;
+        }
     }
 
     bool useResource() {
         if (_token) {
-            std::cout << "Process " << _rank << " used the resource." << std::endl;
-            MPI_Send(&token, 1, MPI_UNSIGNED, 0, TAG_GIVEBACK, MPI_COMM_WORLD);
+            std::cout << "P" << _rank << ": used the resource." << std::endl;
+            MPI_Send(&_token, 1, MPI_UNSIGNED, 0, TAG_GIVEBACK, MPI_COMM_WORLD);
+            _token = 0;
         } else {
-            std::cout << "Process " << _rank << " waiting for token..." << std::endl;
-            MPI_Recv(&token, 1, MPI_UNSIGNED, 0, TAG_SEND, MPI_COMM_WORLD, &st);
+            std::cout << "P" << _rank << ": waiting for token..." << std::endl;
+            unsigned ask = 1;
+            MPI_Send(&ask, 1, MPI_UNSIGNED, 0, TAG_ASK, MPI_COMM_WORLD);
+            MPI_Recv(&_token, 1, MPI_UNSIGNED, 0, TAG_SEND, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            std::cout << "P" << _rank << ": received token." << std::endl;
         }
         return true;
     }
 
     bool doOtherStuff() {
-        std::cout << "Process " << _rank << " doing other stuff." << std::endl;
+        //std::cout << "P" << _rank << ": doing other stuff." << std::endl;
+        double t1 = MPI_Wtime();
+        double t2 = MPI_Wtime();
+        while (t2 < t1+0.02) {
+            t2 = MPI_Wtime();
+        }
         return false;
     }
 
